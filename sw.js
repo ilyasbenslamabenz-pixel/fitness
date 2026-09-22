eeeb98ce8b3142b2cf3f315d4899afab58768de6
GE-fais <ilyas.benslamabenz@gmail.com>
2026-09-22 22:32:35 +0200

    Met à jour le service worker EVO Fit Coach

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='evo-fit-coach-v18';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])).then(()=>self.skipWaiting())));
+const CACHE='evo-fit-coach-v22';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./ilyas-icon.jpg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
