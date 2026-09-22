03fc77ae5dc98e3356a7c17da08d19b8eae59d17
GE-fais <ilyas.benslamabenz@gmail.com>
2026-09-22 22:55:03 +0200

    Actualise le cache après ajout du scanner

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='evo-fit-coach-v22';
+const CACHE='evo-fit-coach-v23';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./ilyas-icon.jpg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
